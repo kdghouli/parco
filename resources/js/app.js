f578d3aaa249d00736fa52b7f0ef22ce4a6b7dee
kdghouli <kdghouli@gmail.com>
2021-11-18 14:08:46 +0000

    khalid pc 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
 //import app from './App.vue';
 
@@ -21,9 +21,12 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('khalid', require('./components/khalid.vue').default);
-Vue.component('index-vhl', require('./components/IndexVhl.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+Vue.component("khalid", require("./components/khalid.vue").default);
+Vue.component("index-vhl", require("./components/IndexVhl.vue").default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -31,5 +34,5 @@ Vue.component('index-vhl', require('./components/IndexVhl.vue').default);
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
 });
